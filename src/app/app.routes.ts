1da4e1f28beeaa7149ce94fec9092147dc05b78e
Ricardo Velasco Salazar <ricardo.pro@outlook.com>
2019-11-25 07:04:46 -0600

    register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graphs1Component } from './pages/graphs1/graphs1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 const appRoutes: Routes = [
     {path: '', 
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
     },
     
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: DashboardComponent},
+    {path: 'register', component: RegisterComponent},
     {path: '**', component: NopagefoundComponent}
 ];
 
